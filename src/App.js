69663cde2693444216381218a9a7ea75073d238b
Chakrapani <100181778+chakrapani-sekar@users.noreply.github.com>
2022-09-15 11:22:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className='container'>
+      <h1>Login</h1>
+    <input type= "text" id= "one" placeholder ="Username"></input>
+    <input type= "Password" id="two" placeholder ="Password"></input> 
+    <button id = "btn" >Submit</button>
+    <p>Create </p>
+    </div>
+    </div>
+  );
+}
+
+export default App;
